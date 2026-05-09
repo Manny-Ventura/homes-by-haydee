1f735d61f47d1bcddced8313b08dc1bf59593f9b
Manny-Ventura <wow.mannyv@gmail.com>
2026-05-09 09:38:56 -0400

    fixed en being default routing by moving proxy.ts to root

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,5 +3,7 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
     locales: ['en', 'es'],
 
-    defaultLocale: 'en'
+    defaultLocale: 'en',
+    localePrefix: "always"
+
 });
